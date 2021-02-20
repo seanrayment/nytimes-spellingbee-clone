ae04c25db0e805f80df85eb867defcbeaabfc034
seanrayment <rayment.code@gmail.com>
2021-02-20 11:43:08 -0500

    click animation

--- src/components/LetterBox.vue
+++ src/components/LetterBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg @mousedown="letterClicked" class="letter-box" viewBox="0 0 120 103.92304845413263">
+  <svg @mousedown="letterClicked" @animationend="resetAnimation" class="letter-box" v-bind:class="{diffanimate: animated}" viewBox="0 0 120 103.92304845413263">
     <polygon
       class="cell-fill"
       points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263 30,103.92304845413263"
@@ -16,73 +16,97 @@
 export default {
   name: "LetterBox",
   props: ["letter"],
+  data: function () {
+    return {
+    animated: false,
+    };
+  },
   methods: {
     letterClicked: function () {
+      this.animated = true;
       console.log("clicked");
       this.$emit("letter-clicked", this.letter);
     },
+
+    resetAnimation: function() {
+      this.animated = false;
+    }
   },
 };
 </script>
 
 <style>
-.letter-box {
-  position: absolute;
-  top: calc(100% / 3);
-  left: 30%;
-  width: 40%;
-  height: calc(100% / 3);
-  fill: #ccc;
-}
+  .letter-box {
+    position: absolute;
+    top: calc(100% / 3);
+    left: 30%;
+    width: 40%;
+    height: calc(100% / 3);
+    fill: #ccc;
+  }
 
-.letter-box:hover {
-  cursor: pointer;
-}
+  .letter-box:hover {
+    cursor: pointer;
+  }
 
-.cell-fill:hover {
-  width: 38%;
-  transition: width 0.2s ease-out;
-}
+  .diffanimate {
+    animation-name: pop;
+    animation-duration: .2s;
+  }
 
-.cell-letter {
-  fill: #000;
-  font-weight: 700;
-  font-size: 24px;
-  text-anchor: middle;
-  text-transform: uppercase;
-  pointer-events: none;
-  user-select: none;
-}
+  @keyframes pop {
+    0%   {
+      height: calc(100% / 3.1);
+    }
+    100% {
+      height: calc(100% / 3);
+    }
+  }
 
-.letter-box:nth-child(1) {
-  transform: translate(0, 0);
-}
+  .cell-fill:hover {
+    width: 38%;
+    transition: width 0.2s ease-out;
+  }
 
-.letter-box:nth-child(2) {
-  transform: translate(-75%, -50%);
-}
+  .cell-letter {
+    fill: #000;
+    font-weight: 700;
+    font-size: 24px;
+    text-anchor: middle;
+    text-transform: uppercase;
+    pointer-events: none;
+    user-select: none;
+  }
 
-.letter-box:nth-child(3) {
-  transform: translate(0, -100%);
-}
+  .letter-box:nth-child(1) {
+    transform: translate(0, 0);
+  }
 
-.letter-box:nth-child(4) {
-  transform: translate(75%, -50%);
-}
+  .letter-box:nth-child(2) {
+    transform: translate(-75%, -50%);
+  }
 
-.letter-box:nth-child(5) {
-  transform: translate(75%, 50%);
-}
+  .letter-box:nth-child(3) {
+    transform: translate(0, -100%);
+  }
 
-.letter-box:nth-child(6) {
-  transform: translate(0, 100%);
-}
+  .letter-box:nth-child(4) {
+    transform: translate(75%, -50%);
+  }
 
-.letter-box:nth-child(7) {
-  transform: translate(-75%, 50%);
-}
+  .letter-box:nth-child(5) {
+    transform: translate(75%, 50%);
+  }
 
-.center-tile {
-  fill: #85b3ff;
-}
+  .letter-box:nth-child(6) {
+    transform: translate(0, 100%);
+  }
+
+  .letter-box:nth-child(7) {
+    transform: translate(-75%, 50%);
+  }
+
+  .center-tile {
+    fill: #85b3ff;
+  }
 </style>
